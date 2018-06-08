954b6f9127ce47ccb40fb9fcae9038608c91c2dd
Ignacio Tula <itula@logos.net.ar>
2018-06-08 04:52:21 -0300

    Funcionalidad de tablero y turnos
    Se agregaron los moviminetos para los 2 jugadores (limitandose a cada tablero).
    Se crearon subrutinas que manejan el turno, es decir quien esta moviendo, y quien deveria mover luego

--- batallaNaval.asm
+++ batallaNaval.asm
@@ -57,6 +57,8 @@ botes_Jug2 db "**##***********"
            db "#******#*******" 
 
 filas db 7
+ComparaPosicion db 0
+MueveJugador db 0
 
 
 
@@ -142,62 +144,232 @@ int 10h
        
         
 
-jmp main
+jmp mueveJug1
 
-main:   mov ah, 00h
+
+; ESTE LOOP ESCUCHA QUE TECLA PRESIONA EL JUGADOR 1
+mueveJug1:   
+        mov ah, 00h
         int 16h
                         
-        cmp al, 73h     ;Compara que tecla se presiono, si es 's'    
-        je Down
+        cmp al, 78h     ;Compara que tecla se presiono, si es 's'    
+        je DetectarAbajoJug1
                 
         cmp al, 77h     ;Compara que tecla se presiono, si es 'w'    
-        je Up
+        je DetectarArribaJug1
                 
         cmp al, 61h     ;Compara que tecla se presiono, si es 'a'    
-        je Left
+        je DeteccionIzquierdaJug1
                 
         cmp al, 64h     ;Compara que tecla se presiono, si es 'd'    
-        je Right
+        je DeteccionDerechaJug1
+        
+        cmp al, 73h     ;Compara que tecla se presiono, si es 's'    
+        je DisparoJugador1
                 
        
                 
-        jmp main        ;Sino, no hace nada y vuelve a pedir una tecla  
+        jmp mueveJug1        ;Sino, no hace nada y vuelve a pedir una tecla  
                     
       
+ 
+ 
+ ; ESTE LOOP ESCUCHA QUE TECLA PRESIONA EL JUGADOR 2   
+ ; !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!CAMBIAR LAS SUB DE ESTE
+mueveJug2:   
+        mov ah, 00h
+        int 16h
+                        
+        cmp al, 78h     ;Compara que tecla se presiono, si es 'x'    
+        je DetectarAbajoJug2
+                
+        cmp al, 77h     ;Compara que tecla se presiono, si es 'w'    
+        je DetectarArribaJug2
+                
+        cmp al, 61h     ;Compara que tecla se presiono, si es 'a'    
+        je DeteccionIzquierdaJug2
+                
+        cmp al, 64h     ;Compara que tecla se presiono, si es 'd'    
+        je DeteccionDerechaJug2
+        
+        cmp al, 73h     ;Compara que tecla se presiono, si es 's'    
+        je DisparoJugador2        
+       
+                
+        jmp mueveJug2        ;Sino, no hace nada y vuelve a pedir una tecla  
+                    
+
+ 
+
+
+; CALCULA QUE MOVIMIENTOS SON POSIBLE PARA EL JUGADOR1
+
    
-    
-Right:
-        add dl, 1           ;para reposicionar el cursor columna
-        call SetCursor      ;llamo al procedimiento para setear cursor
-        jmp main
-        ret
+            
+DeteccionDerechaJug1:
+        mov ComparaPosicion, dl
+        sub ComparaPosicion, 14 ; No puede ir a derecha de la columna 14
+        JS MoverDerecha         ; si la diferencia es negativa, se puede mover a derecha
+        jmp mueveJug1
+        ret            
+            
+            
 
-Left:
-        sub dl, 1           ;para reposicionar el cursor columna
-        call SetCursor      ;llamo al procedimiento para setear cursor
-        jmp main
-        ret
+DeteccionIzquierdaJug1:
+        cmp dl, 0               ; No puede ir a izquierda si ya esta en columna 0
+        je mueveJug1            ; si son iguales, volver al loop
+        jmp MoverIzquierda      ; si no son iguales, permite el movimiento
+        ret   
+        
+                
+        
 
-Up:
+DetectarArribaJug1:
+        mov ComparaPosicion, dh    ; No puede ir hacia arriba si ya esta en fila 10 (0A)
+        sub ComparaPosicion, 10    ; Si la resta es positiva permite subir
+        JNS MoverArriba
+        jmp mueveJug1
+        ret              
+             
+            
+        
+        
+
+
+DetectarAbajoJug1:
+        mov ComparaPosicion, dh    ; No puede ir hacia abajo si ya esta en fila 19 (0A)
+        sub ComparaPosicion, 19    ; Si la resta es negativa permite bajar
+        JS MoverAbajo
+        jmp mueveJug1
+        ret
+        
+DisparoJugador1:
+        jmp FinDeTurno   
+
+
+
+; CALCULA QUE MOVIMIENTOS SON POSIBLE PARA EL JUGADOR2
+
+   
+            
+DeteccionDerechaJug2:
+        mov ComparaPosicion, dl
+        sub ComparaPosicion, 31 ; No puede ir a derecha de la columna 14
+        JS MoverDerecha         ; si la diferencia es negativa, se puede mover a derecha
+        jmp mueveJug2
+        ret            
+            
+            
+
+DeteccionIzquierdaJug2:
+        mov ComparaPosicion, dl
+        sub ComparaPosicion, 11               ; No puede ir a izquierda si ya esta en columna 0
+        jns mueveJug2            ; si son iguales, volver al loop
+        jmp MoverIzquierda      ; si no son iguales, permite el movimiento
+        ret   
+        
+                
+        
+
+DetectarArribaJug2:
+        mov ComparaPosicion, dh    ; No puede ir hacia arriba si ya esta en fila 10 (0A)
+        sub ComparaPosicion, 10    ; Si la resta es positiva permite subir
+        JNS MoverArriba
+        jmp mueveJug2
+        ret              
+             
+            
+        
+        
+
+
+DetectarAbajoJug2:
+        mov ComparaPosicion, dh    ; No puede ir hacia abajo si ya esta en fila 19 (0A)
+        sub ComparaPosicion, 19    ; Si la resta es negativa permite bajar
+        JS MoverAbajo
+        jmp mueveJug2
+        ret   
+
+DisparoJugador2:
+        jmp FinDeTurno
+
+
+
+
+
+        
+
+; subrutinas  (o loops) que se encargan del absoluto movimiento del cursor
+
+MoverDerecha:
+        add dl, 1           
+        call SetCursor           
+        jmp SigueMoviendo
+        ret 
+
+        
+MoverIzquierda:
+        sub dl, 1           
+        call SetCursor      
+        jmp SigueMoviendo
+        ret                    
+        
+MoverArriba:
         sub dh, 1           ;para reposicionar el cursor fila
         call SetCursor      ;llamo al procedimiento para setear cursor
-        jmp main
-        ret
-
-Down:
+        jmp SigueMoviendo
+        ret        
+                
+MoverAbajo:
         add dh, 1           ;para reposicionar el cursor fila
         call SetCursor      ;llamo al procedimiento para setear cursor
-        jmp main
-        ret
-      
-      
-      
+        jmp SigueMoviendo
+        ret             
+
 SetCursor proc              
         mov ah, 02h
         mov bh, 00
         int 10h             ;Analizar esta int, que es la que posiciona el cursor
         ret
 
-SetCursor endp
+SetCursor endp 
 
 
+
+
+
+; Subrutinas de cambio de turno
+
+SigueMoviendo:
+    cmp MueveJugador,0
+    je  mueveJug1
+    jmp mueveJug2
+    ret
+
+
+FinDeTurno:
+    cmp MueveJugador,0
+    je  AhoraMueveJugador2
+    jmp AhoraMueveJugador1
+    ret
+
+
+AhoraMueveJugador1:
+
+    mov MueveJugador,0
+    mov dh, 9     ; Ubicar cursor en posicion 0,0 de su matriz
+    mov dl, 0        
+    mov ah, 02h
+    int 10h 
+    jmp mueveJug1
+    ret
+    
+
+AhoraMueveJugador2:
+    mov MueveJugador,1
+    mov dh, 9     ; Ubicar cursor en posicion 0,11 de su matriz
+    mov dl, 17        
+    mov ah, 02h
+    int 10h
+    jmp mueveJug2
+    ret
